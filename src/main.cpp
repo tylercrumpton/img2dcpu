9f72c6f1ad3b069aae203f7f6d81babb5b3eed16
Tyler Crumpton <tac0010@auburn.edu>
2012-04-26 23:48:58 -0500

    Notably reduced output code size. Not up to 1.5 spec.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
   This utility is licensed under a GPLv3 License (see COPYING).
   Source at: https://github.com/tac0010/img2dcpu
 
+  April 26, 2012 - v0.7: Notably reduced output code size. Not up to 1.5 spec.
   April 20, 2012 - v0.6: Added cross-platform support, fixed 32x24 animation.
   April 19, 2012 - v0.5: Added animation support to all three resolutions.
   April 17, 2012 - v0.4: Added support for 64x64 centered images.
@@ -75,6 +76,8 @@ string generateLowResTile(int index, RGBTRIPLE firstPixel, RGBTRIPLE secondPixel
 string int2hex(int i, int width);
 int roundColorValue(RGBTRIPLE color);
 string genFontSpace(int imageMode);
+string generateDCPUFull();
+string generateDCPUSmall();
 string generateHighResFullTile(int pxIndex, int imgIndex);
 string generateHighResSmallTile(int pxIndex, int imgIndex);
 
@@ -206,95 +209,187 @@ void readImage(char *filename) {
 
 //Generates and saves DCPU code from the "image[]" array.
 void saveFile(char *filename) {
-    int frameWidth =0;
-    int frameHeight = 0;
-    if (imageMode == LOW_RES_FULL) {
-        frameWidth = LOW_RES_FULL_W;
-        frameHeight = LOW_RES_FULL_H;
-    }
-    else if (imageMode == HIGH_RES_FULL) {
-        frameWidth = HIGH_RES_FULL_W;
-        frameHeight = HIGH_RES_FULL_H;
-    }
-    else if (imageMode == HIGH_RES_SMALL) {
-        frameWidth = HIGH_RES_SMALL_W;
-        frameHeight = HIGH_RES_SMALL_H;
-    }
-
-    int frames = bih.biWidth / frameWidth;
 
     ofstream oFile;
     oFile.open(filename); //Open file for writing (overwrites file)
 
-    oFile << genFontSpace(imageMode); //Set up custom font
+    if (imageMode == HIGH_RES_SMALL) {
+        oFile << generateDCPUSmall();
+    }
+    else {
+        oFile << generateDCPUFull();
+    }
+
+    oFile.close(); //Close the file
+}
+
+string generateDCPUFull() {
+    stringstream output;
+    int frameWidth = 0;
+    if (imageMode == LOW_RES_FULL) {
+        frameWidth = LOW_RES_FULL_W;
+    }
+    else if (imageMode == HIGH_RES_FULL) {
+        frameWidth = HIGH_RES_FULL_W;
+    }
+
+    int frames = bih.biWidth / frameWidth;
+
+    output << "SET A, 0\n" <<
+              "SET B, font_space\n" <<
+              ":font_loop\n" <<
+              "SET [0x8180 + A], [B]\n" <<
+              "ADD A, 1\n" <<
+              "ADD B, 1\n";
+
+    if (imageMode == LOW_RES_FULL) {
+        output << "IFN A, 2\n";
+    }
+    else if (imageMode == HIGH_RES_FULL) {
+        output << "IFN A, 256\n";
+    }
+
+    output << "SET PC, font_loop\n\n" <<
+              ":full_loop\n" <<
+              "SET B, tile_space\n" <<
+              "SET C, " << frames << "\n";
+
 
     if (animationFlag == true) {
-        oFile << ":anim_loop\n";
+        output << ":frame_loop\n";
     }
+
+    output << "SET A, 0\n" <<
+              ":tile_loop\n" <<
+              "SET [0x8000 + A], [B]\n" <<
+              "ADD A, 1\n" <<
+              "ADD B, 1\n" <<
+              "IFN A, 0x0180\n" <<
+              "SET PC, tile_loop\n\n";
+
+    if (animationFlag == true) {
+        output << "SUB C, 1\n" <<
+                  "JSR delay\n" <<
+                  "IFE C, 0\n" <<
+                  "SET PC, full_loop\n" <<
+                  "SET PC, frame_loop\n\n" <<
+                  ":delay\n" <<
+                  "SET X, 0\n" <<
+                  ":loop\n" <<
+                  "ADD X, 1\n" <<
+                  "IFN X, 1000\n" <<
+                  "SET PC, loop\n" <<
+                  "SET PC, POP\n";
+    }
+
+    output << "BRK\n";
+
+    output << ":font_space DAT ";
+    output << genFontSpace(imageMode); //Set up custom font
+
+    output << "\n:tile_space DAT ";
+
+
 
     for (int x=0;x<frames;++x) {
         //Calculate the DCPU code for each "pixel" (tile)
         //Skip every other row, because we take 2 at a time.
         if (imageMode == LOW_RES_FULL) {
-            for (int i=0; i<frameHeight - 1; i+=2) {
+            for (int i=0; i<bih.biHeight - 1; i+=2) {
                 for (int j=0; j<frameWidth; ++j) {
                     int pxIndex = (32 * (i/2)) + j; //Index of the DCPU pixel (or tile);
-                    int imgIndex = (bih.biWidth * (frameHeight - (i+1) )) + j + (x * frameWidth); //Index of pixel in BMP
-                    oFile << generateLowResTile(pxIndex, image[imgIndex], image[imgIndex - bih.biWidth]);
+                    int imgIndex = (bih.biWidth * (bih.biHeight - (i+1) )) + j + (x * frameWidth); //Index of pixel in BMP
+                    output << generateLowResTile(pxIndex, image[imgIndex], image[imgIndex - bih.biWidth]);
                 }
             }
         }
         else if (imageMode == HIGH_RES_FULL) {
-            for (int i=0; i<frameHeight - 3; i+=4) {
+            for (int i=0; i<bih.biHeight - 3; i+=4) {
                 for (int j=0; j<frameWidth - 1; j+=2) {
                     int pxIndex = (32 * (i/4)) + j/2; //Index of the DCPU pixel (or tile)
-                    int imgIndex = (bih.biWidth * (frameHeight - (i+1) )) + j + (x * frameWidth); //Index of pixel in BMP
+                    int imgIndex = (bih.biWidth * (bih.biHeight - (i+1) )) + j + (x * frameWidth); //Index of pixel in BMP
                     //Analyze tile:
-                    oFile << generateHighResFullTile(pxIndex, imgIndex);
+                    output << generateHighResFullTile(pxIndex, imgIndex);
                 }
             }
         }
-        else if (imageMode == HIGH_RES_SMALL) {
-            for (int i=0; i<frameHeight - 7; i+=8) {
-                for (int j=0; j<frameWidth - 3; j+=4) {
-                    int pxIndex = 2 * ((16 * (i/8)) + j/4); //Index of the DCPU pixel (or tile)
-                    int imgIndex = (bih.biWidth * (frameHeight - (i+1) )) + j + (x * frameWidth); //Index of pixel in BMP
-                    //Analyze tile:
-                    oFile << generateHighResSmallTile(pxIndex, imgIndex);
-                }
-            }
-        }
-        oFile << "JSR delay\n";
     }
 
-    if (animationFlag == true) {
-        oFile << "SET PC, anim_loop\n";
-    }
-
-    oFile << ":delay\n";
-    oFile << "SET A, 0;\n";
-    oFile << ":loop\n";
-    oFile << "ADD A, 1;\n";
-    oFile << "IFN A, 1000;\n";
-    oFile << "SET PC, loop\n";
-    oFile << "SET PC, POP\n";
-
-    oFile.close(); //Close the file
+    return output.str();
 }
 
-//Generates DCPU code for two vertically adjacent BMP pixels (one DCMP tile).
-string generateLowResTile(int index, RGBTRIPLE firstPixel, RGBTRIPLE secondPixel) {
-    string output;
+//NON WORKING
+string generateDCPUSmall() {
+    stringstream output;
+    int frameWidth = HIGH_RES_SMALL_W;
+    int frames = bih.biWidth / frameWidth;
 
-    //Find the closest allowable hex value for each color:
-    int fRGB = roundColorValue(firstPixel);
-    int sRGB = roundColorValue(secondPixel);
+    output << "SET A, 0\n" <<
+              "SET B, tile_space\n" <<
+              ":skip_loop\n" <<
+              "SET C, 0\n" <<
+              "ADD A, 16\n" <<
+              "IFE A, 272\n" <<
+              "SET PC, full_loop\n" <<
+              ":tile_loop\n" <<
+              "SET [0x8038 + A], [B]\n" <<
+              "ADD A, 1\n" <<
+              "ADD B, 1\n" <<
+              "ADD C, 1\n" <<
+              "IFE C, 16\n" <<
+              "SET PC, skip_loop\n" <<
+              "SET PC, tile_loop\n" <<
+              ":full_loop\n" <<
+              "SET B, font_space\n";
 
-    //Create DCPU code in the form of "SET [0x8AAA], 0xFS00".
-    //AAA = Address offset, F = RGB of top pixel, S = RGB of bottom pixel
-    output = "SET [0x8" + int2hex(index, 3) + "], 0x" + int2hex(fRGB, 1) + int2hex(sRGB, 1) + "00\n";
 
-    return output;
+
+    if (animationFlag == true) {
+        output << "SET C, " << frames << "\n" <<
+                  ":frame_loop\n";
+    }
+
+    output << "SET A, 0\n" <<
+              ":font_loop\n" <<
+              "SET [0x8180 + A], [B]\n" <<
+              "ADD A, 1\n" <<
+              "ADD B, 1\n" <<
+              "IFN A, 256\n" <<
+              "SET PC, font_loop\n\n";
+
+    if (animationFlag == true) {
+        output << "SUB C, 1\n" <<
+                  "JSR delay\n" <<
+                  "IFE C, 0\n" <<
+                  "SET PC, full_loop\n" <<
+                  "SET PC, frame_loop\n\n" <<
+                  ":delay\n" <<
+                  "SET X, 0\n" <<
+                  ":loop\n" <<
+                  "ADD X, 1\n" <<
+                  "IFN X, 1000\n" <<
+                  "SET PC, loop\n" <<
+                  "SET PC, POP\n";
+    }
+
+    output << "BRK\n";
+
+    output << ":tile_space DAT ";
+    output << genFontSpace(imageMode); //Set up custom font
+
+    output << "\n:font_space DAT ";
+
+    for (int x=0;x<frames;++x) {
+        for (int i=0; i<bih.biHeight - 7; i+=8) {
+            for (int j=0; j<frameWidth - 3; j+=4) {
+                int pxIndex = 2 * ((16 * (i/8)) + j/4); //Index of the DCPU pixel (or tile)
+                int imgIndex = (bih.biWidth * (bih.biHeight - (i+1) )) + j + (x * frameWidth); //Index of pixel in BMP
+                //Analyze tile:
+                output << generateHighResSmallTile(pxIndex, imgIndex);
+            }
+        }
+    }
+    return output.str();
 }
 
 //Converts and integer to a HEX string of a certain width.
@@ -334,23 +429,20 @@ string genFontSpace(int imageMode) {
 
     //Set up custom low res font space:
     if (imageMode == LOW_RES_FULL) {
-
-        stream << "SET [0x8180], 0x0f0f\n";
-        stream << "SET [0x8181], 0x0f0f\n";
+        stream << "0x0f0f, 0x0f0f\n";
     }
 
     //Set up custom full high res font space:
     else if (imageMode == HIGH_RES_FULL) {
         int decValues[4] = {0, 3, 12, 15};
-        int count = 0;
         for (int i=0; i<4; ++i) {
             for (int j=0; j<4; ++j) {
                 for (int k=0; k<4; ++k) {
                     for (int l=0; l<2; ++l) {
                         string ij = int2hex(decValues[i], 1) + int2hex(decValues[j], 1);
                         string kl = int2hex(decValues[k], 1) + int2hex(decValues[l], 1);
-                        stream << "SET [0x8" + int2hex(384 + count++, 3) + "], 0x" + ij + ij + "\n";
-                        stream << "SET [0x8" + int2hex(384 + count++, 3) + "], 0x" + kl + kl + "\n";
+                        stream << "0x" + ij + ij + ", ";
+                        stream << "0x" + kl + kl + ", ";
                     }
                 }
             }
@@ -361,9 +453,8 @@ string genFontSpace(int imageMode) {
     else if (imageMode == HIGH_RES_SMALL) {
         for (int i=0; i<8; ++i) {
             for (int j=0; j<16; ++j) {
-                int tileIndex = centerOffset + i*32 + j;
                 int charIndex = i*16 + j;
-                stream << "SET [0x8" + int2hex(tileIndex, 3) + "], 0x0f" + int2hex(charIndex, 2) + "\n";
+                stream << "0x0f" + int2hex(charIndex, 2) + ", ";
             }
         }
     }
@@ -371,9 +462,23 @@ string genFontSpace(int imageMode) {
   return stream.str();
 }
 
+//Generates DCPU code for two vertically adjacent BMP pixels (one DCMP tile).
+string generateLowResTile(int index, RGBTRIPLE firstPixel, RGBTRIPLE secondPixel) {
+    string output;
+
+    //Find the closest allowable hex value for each color:
+    int fRGB = roundColorValue(firstPixel);
+    int sRGB = roundColorValue(secondPixel);
+
+    output = "0x" + int2hex(fRGB, 1) + int2hex(sRGB, 1) + "00, ";
+
+    return output;
+}
+
 //Generates the High Res Full Size tile when using the higher resolution 7-bit font width
 string generateHighResFullTile(int pxIndex, int imgIndex) {
     boolean invertFlag;
+    string output;
     int i, j, k, l;
 
     if (roundColorValue(image[(imgIndex+1) - bih.biWidth]) == 0) {
@@ -503,11 +608,17 @@ string generateHighResFullTile(int pxIndex, int imgIndex) {
     int character =  32*i + 8*j + 2*k + l;
     //Invert the tile is needed:
     if (invertFlag == true) {
-        return "SET [0x8" + int2hex(pxIndex, 3) + "], 0x0f" + int2hex(character, 2) + "\n";
+        //return "SET [0x8" + int2hex(pxIndex, 3) + "], 0x0f" + int2hex(character, 2) + "\n";
+        output = "0x0f" + int2hex(character, 2) + ", ";
+
+
     }
     else {
-        return "SET [0x8" + int2hex(pxIndex, 3) + "], 0xf0" + int2hex(character, 2) + "\n";
+        //return "SET [0x8" + int2hex(pxIndex, 3) + "], 0xf0" + int2hex(character, 2) + "\n";
+        output = "0xf0" + int2hex(character, 2) + ", ";
     }
+
+    return output;
 }
 
 //Generates the High Res Small Size tile when using the higher resolution 7-bit font width
@@ -522,6 +633,6 @@ string generateHighResSmallTile(int pxIndex, int imgIndex) {
             }
         }
     }
-    return "SET [0x8" + int2hex(384 + pxIndex, 3) + "], 0x" + int2hex(ijkl[0], 2) + int2hex(ijkl[1], 2) + "\n" +
-           "SET [0x8" + int2hex(385 + pxIndex, 3) + "], 0x" + int2hex(ijkl[2], 2) + int2hex(ijkl[3], 2) + "\n";
+    return "0x" + int2hex(ijkl[0], 2) + int2hex(ijkl[1], 2) + ", " +
+           "0x" + int2hex(ijkl[2], 2) + int2hex(ijkl[3], 2) + ", ";
 }
